35857cb4a6ed7196349b999b3d13fd3023f028eb
thierno sadou diallo <thierved01@gmail.com>
2024-12-04 20:16:00 +0000

    finnished the basics styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello BackKKKKKKK!</div>;
+  return (
+    <div className="App">
+      <div className="text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est deserunt
+        error aut eum mollitia ea facere nobis autem nesciunt ipsum!
+      </div>
+      <div className="input">
+        <input type="text" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
